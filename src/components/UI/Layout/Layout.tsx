a7988204101510cb9f1cfdf676397d805d94271d
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-23 18:51:43 +0100

    Add component for layout

---
+++ src/components/UI/Layout/Layout.tsx
@@ -0,0 +1,31 @@
+import { Grid } from "@chakra-ui/react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <Grid
+      as="main"
+      className="layout-grid"
+      paddingInline="3vw"
+      templateColumns="repeat(12, 1fr)"
+      minHeight="100vh"
+      gap={"1.95vw"}
+      border={{
+        base: "2px solid purple",
+        md: "2px solid yellow",
+        lg: "2px solid red",
+        xl: "2px solid blue",
+        "2xl": "2px solid green",
+      }}
+      bgColor="brand.darkBlue"
+      color="white"
+    >
+      {children}
+    </Grid>
+  );
+};
+
+export default Layout;
