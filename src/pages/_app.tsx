96fae3cf3c60b01c940bded575f77425497045f0
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-23 12:48:41 +0100

    Add 'outfit' font to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,13 @@
 import "@/styles/globals.css";
+import "@fontsource/outfit/100.css";
+import "@fontsource/outfit/200.css";
+import "@fontsource/outfit/300.css";
+import "@fontsource/outfit/400.css";
+import "@fontsource/outfit/500.css";
+import "@fontsource/outfit/600.css";
+import "@fontsource/outfit/700.css";
+import "@fontsource/outfit/800.css";
+import "@fontsource/outfit/900.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import theme from "../styles/theme";
