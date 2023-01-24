44527f1282662e0f843403dc91bcfecd51048d6b
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-24 14:44:57 +0100

    Switch to two column layout on large devices

--- src/components/UI/Layout/Layout.tsx
+++ src/components/UI/Layout/Layout.tsx
@@ -11,7 +11,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       className="layout-grid"
       paddingInline={{ lg: "3vw" }}
       paddingTop={{ md: "1.5625rem", lg: "unset" }}
-      templateColumns={{ lg: "repeat(12, 1fr)" }}
+      // templateColumns={{ lg: "repeat(12, 1fr)" }}
+      templateColumns={{ lg: "6.67vw 1fr" }}
       templateRows={{ base: "4.5rem 1fr", lg: "unset" }}
       minHeight="100vh"
       gap={{ base: "1.25rem", lg: "1.95vw" }}
