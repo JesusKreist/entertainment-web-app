6256ef8fadaca02c40ca77ec56c163b9269f9b35
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-24 11:22:03 +0100

    Edit height and width of nav bar links on lg devices

--- src/components/NavBar/NavLinks.tsx
+++ src/components/NavBar/NavLinks.tsx
@@ -4,26 +4,51 @@ import NavLinkButton from "../Icons/NavLinkButton";
 
 const NavLinks = () => {
   return (
-    <Flex
-      direction="column"
-      alignItems="center"
-      className="nav-links"
-      border="1px solid white"
-      height={{ xl: "200px", "2xl": "26%" }}
-      justifyContent="space-between"
-      style={{ aspectRatio: "0.1" }}
-    >
-      <NavLinkButton imageName="icon-nav-home.svg" altText="home icon" />
-      <NavLinkButton imageName="icon-nav-movies.svg" altText="movies icon" />
-      <NavLinkButton
-        imageName="icon-nav-tv-series.svg"
-        altText="tv series icon"
-      />
-      <NavLinkButton
-        imageName="icon-nav-bookmark.svg"
-        altText="bookmark icon"
-      />
-    </Flex>
+    <>
+      <Flex
+        display={{ base: "none", xl: "flex" }}
+        direction="column"
+        alignItems="center"
+        className="nav-links-xl"
+        //   border="1px solid white"
+        height={{ xl: "200px", "2xl": "26%" }}
+        justifyContent="space-between"
+        style={{ aspectRatio: "0.1" }}
+      >
+        <NavLinkButton imageName="icon-nav-home.svg" altText="home icon" />
+        <NavLinkButton imageName="icon-nav-movies.svg" altText="movies icon" />
+        <NavLinkButton
+          imageName="icon-nav-tv-series.svg"
+          altText="tv series icon"
+        />
+        <NavLinkButton
+          imageName="icon-nav-bookmark.svg"
+          altText="bookmark icon"
+        />
+      </Flex>
+
+      <Flex
+        direction="column"
+        alignItems="center"
+        className="nav-links-base-to-lg"
+        //   border="1px solid white"
+        height={{ lg: "12.5rem" }}
+        width={{ lg: "1rem" }}
+        justifyContent="space-between"
+        display={{ base: "flex", xl: "none" }}
+      >
+        <NavLinkButton imageName="icon-nav-home.svg" altText="home icon" />
+        <NavLinkButton imageName="icon-nav-movies.svg" altText="movies icon" />
+        <NavLinkButton
+          imageName="icon-nav-tv-series.svg"
+          altText="tv series icon"
+        />
+        <NavLinkButton
+          imageName="icon-nav-bookmark.svg"
+          altText="bookmark icon"
+        />
+      </Flex>
+    </>
   );
 };
 
