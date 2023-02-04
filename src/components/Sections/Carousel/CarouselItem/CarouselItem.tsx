a54486013c8094ed6ee6c7a27f6b0794f97c8923
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-02-04 14:23:04 +0100

    Move bookmark button on carousel item to separate component

--- src/components/Sections/Carousel/CarouselItem/CarouselItem.tsx
+++ src/components/Sections/Carousel/CarouselItem/CarouselItem.tsx
@@ -1,5 +1,6 @@
 import { Grid, Flex, Image, Box, Text } from "@chakra-ui/react";
 import React from "react";
+import BookmarkButton from "./BookmarkButton";
 import CarouselItemText from "./CarouselItemText/CarouselItemText";
 
 interface CarouselItemProps {
@@ -32,32 +33,7 @@ const CarouselItem: React.FC<CarouselItemProps> = ({
       templateRows="repeat(16, minmax(0, 1fr))"
       templateColumns="repeat(16, minmax(0, 1fr))"
     >
-      <Flex
-        // border="1px solid white"
-        gridRow="2 / 5"
-        gridColumn="14 / 16"
-        justifyContent="center"
-        zIndex={1}
-      >
-        <Box
-          as="button"
-          // border="1px solid red"
-          height={{ base: "1.6875rem", md: "2.7rem", "2xl": "3vw" }}
-          sx={{
-            aspectRatio: "1",
-          }}
-          rounded="full"
-          bgColor="rgb(151, 151, 151, 0.5)"
-        >
-          <Image
-            src="assets/figma-bookmark-icon.svg"
-            alt="bookmark icon"
-            height="50%"
-            margin="0 auto"
-          />
-        </Box>
-      </Flex>
-
+      <BookmarkButton />
       <Box className="play-button"></Box>
 
       <CarouselItemText
---
+++ src/components/Sections/Carousel/CarouselItem/CarouselItemImage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CarouselItemImage = () => {
+  return <div>CarouselItemImage</div>;
+};
+
+export default CarouselItemImage;
