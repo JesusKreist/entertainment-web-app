0a334c9de99d7fda8d23021de905edfa04252bde
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-02-04 15:04:52 +0100

    Add starter code for movies, tv-series and bookmarks page categories

---
+++ src/pages/bookmarks.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookmarksPage = () => {
+  return <BookmarksPage />;
+};
+
+export default BookmarksPage;
---
+++ src/pages/tv-series.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TVSeries from "../components/TVSeries.tsx/TVSeries";
+
+const SeriesPage = () => {
+  return <TVSeries />;
+};
+
+export default SeriesPage;
