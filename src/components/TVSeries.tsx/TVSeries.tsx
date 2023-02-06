1d62c18606728d5e73300fef9fe53eaef7187be7
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-02-06 20:43:15 +0100

    Add search functionality to tv-series category

--- src/components/TVSeries.tsx/TVSeries.tsx
+++ src/components/TVSeries.tsx/TVSeries.tsx
@@ -4,12 +4,19 @@ import { usePageStore } from "../../data/appState";
 import { series } from "../../data/data";
 import Gallery from "../Sections/Gallery/Gallery";
 import Section from "../Sections/Section/Section";
+import MainContent from "../UI/Layout/MainContent";
 
 const TVSeries = () => {
   const setPageCategory = usePageStore((state) => state.setPageCategory);
   useEffect(() => {
     setPageCategory("series");
   }, [setPageCategory]);
+
+  const defaultContent = (
+    <Section title="TV Series" overflowX="hidden">
+      <Gallery mediaToDisplay={series} />
+    </Section>
+  );
 
   return (
     <Grid
@@ -28,27 +35,8 @@ const TVSeries = () => {
       paddingLeft={{ base: "1.25rem", lg: "3vw" }}
       rowGap={{ base: "1rem", md: "1rem", lg: "2vw" }}
       overflowY="scroll"
-      // overflowY="hidden"
     >
-      {/* <Section
-        title="Movies"
-        // paddingRight={{ base: "", md: "unset" }}
-        overflowX="scroll"
-        // gridRow={{ lg: "4 / span 6", xl: "4 / span 9", "2xl": "4 / span 9" }}
-      >
-        <Carousel carouselItems={allTrendingMoviesAndShows} />
-      </Section> */}
-
-      <Section
-        title="TV Series"
-        // paddingRight={{ base: "", md: "unset" }}
-        // overflowX="scroll"
-        overflowX="hidden"
-
-        // gridRow={{ lg: "4 / span 6", xl: "4 / span 9", "2xl": "4 / span 9" }}
-      >
-        <Gallery mediaToDisplay={series} />
-      </Section>
+      <MainContent mediaToDisplay={series} defaultContent={defaultContent} />
     </Grid>
   );
 };
