d55fd2642491d1c0b84cc7e82e9afd24ec64f4e3
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-02-20 14:11:31 +0100

    Add signup with session strategy

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,74 +1,150 @@
 // pages/api/auth/[...nextauth].ts
 
-import { NextApiHandler } from "next";
-import NextAuth from "next-auth";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "../../../../lib/prisma";
-import { checkIfUserExists, comparePassword } from "./databaseFunctions";
+import {
+  checkIfUserExists,
+  comparePassword,
+  fromDate,
+  generateSessionToken,
+} from "./databaseFunctions";
+import { setCookie, getCookie } from "cookies-next";
+import { encode, decode } from "next-auth/jwt";
 
-const options = {
-  providers: [
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
+const authHandler: NextApiHandler = async (req, res) => {
+  const options = (
+    req: NextApiRequest,
+    res: NextApiResponse
+  ): NextAuthOptions => {
+    const adapter = PrismaAdapter(prisma);
+
+    return {
+      providers: [
+        CredentialsProvider({
+          // The name to display on the sign in form (e.g. "Sign in with...")
+          name: "Credentials",
+          // `credentials` is used to generate a form on the sign in page.
+          // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+          // e.g. domain, username, password, 2FA token, etc.
+          // You can pass any HTML attribute to the <input> tag through the object.
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "jsmith",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize(credentials, req) {
+            // console.log("credentials :>>", credentials);
+            console.log("####################################################");
+            // console.log("req :>>", req);
+
+            // return null;
+            if (
+              !credentials ||
+              !credentials.username ||
+              !credentials.password
+            ) {
+              return null;
+            }
+
+            const user = await checkIfUserExists(credentials.username);
+            if (!user || !user.passwordHash) {
+              return null;
+            }
+            console.log("user :>>", user);
+
+            const isPasswordCorrect = await comparePassword(
+              credentials.password,
+              user.passwordHash
+            );
+
+            if (!isPasswordCorrect) {
+              return null;
+            }
+
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+            };
+          },
+        }),
+      ],
+      // pages: {
+      //   signIn: "/login",
+      // },
+      adapter: adapter,
+      secret: process.env.NEXTAUTH_SECRET,
+      callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+          // Check if this sign in callback is being called in the credentials authentication flow. If so, use the next-auth adapter to create a session entry in the database (SignIn is called after authorize so we can safely assume the user is valid and already authenticated).
+          if (
+            req.query.nextauth?.includes("callback") &&
+            req.query.nextauth.includes("credentials") &&
+            req.method === "POST"
+          ) {
+            if (user) {
+              const sessionToken = generateSessionToken();
+              const sessionMaxAge = 60 * 60 * 24 * 30; //30Days
+              const sessionExpiry = fromDate(sessionMaxAge);
+
+              await adapter.createSession({
+                sessionToken: sessionToken,
+                userId: user.id,
+                expires: sessionExpiry,
+              });
+
+              setCookie("next-auth.session-token", sessionToken, {
+                expires: sessionExpiry,
+                req: req,
+                res: res,
+              });
+            }
+          }
+
+          return true;
+        },
       },
-      async authorize(credentials, req) {
-        console.log("credentials :>>", credentials);
-        console.log("####################################################");
-        console.log("req :>>", req);
+      jwt: {
+        encode: async ({ token, secret, maxAge }) => {
+          if (
+            req.query.nextauth?.includes("callback") &&
+            req.query.nextauth?.includes("credentials") &&
+            req.method === "POST"
+          ) {
+            const cookie = getCookie("next-auth.session-token", {
+              req: req,
+            }) as string;
 
-        return null;
-        // if (!credentials || !credentials.username || !credentials.password) {
-        //   return null;
-        // }
+            console.log("cookie :>>", cookie);
 
-        // const user = await checkIfUserExists(credentials.username);
-        // if (!user || !user.passwordHash) {
-        //   return null;
-        // }
+            if (cookie) return cookie;
+            else return "";
+          }
+          // Revert to default behaviour when not in the credentials provider callback flow
+          return encode({ token, secret, maxAge });
+        },
+        decode: async ({ token, secret }) => {
+          if (
+            req.query.nextauth?.includes("callback") &&
+            req.query.nextauth.includes("credentials") &&
+            req.method === "POST"
+          ) {
+            return null;
+          }
 
-        // const isPasswordCorrect = await comparePassword(
-        //   credentials.password,
-        //   user.passwordHash
-        // );
-
-        // if (!isPasswordCorrect) {
-        //   return null;
-        // }
-
-        // return {
-        //   id: user.id,
-        //   name: user.name,
-        //   email: user.email,
-        // };
+          // Revert to default behaviour when not in the credentials provider callback flow
+          return decode({ token, secret });
+        },
       },
-    }),
-  ],
-  pages: {
-    signIn: "/login",
-  },
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async session({ session, token }: { session: any; token: any }) {
-      session.address = token.sub;
-      session.user.name = token.sub;
-      session.user.image = "https://www.fillmurray.com/128/128";
-      return session;
-    },
-  },
+    };
+  };
+
+  return await NextAuth(req, res, options(req, res));
 };
-
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
--- src/pages/api/auth/databaseFunctions.ts
+++ src/pages/api/auth/databaseFunctions.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import bcryptjs from "bcryptjs";
+import { randomUUID } from "crypto";
 
 const prisma = new PrismaClient();
 
@@ -27,4 +28,13 @@ export const comparePassword = async (
   const isMatch = await bcryptjs.compare(password, passwordHash);
 
   return isMatch;
+};
+
+export const generateSessionToken = () => {
+  // Use `randomUUID` if available. (Node 15.6++)
+  return randomUUID?.();
+};
+
+export const fromDate = (time: number, date = Date.now()) => {
+  return new Date(date + time * 1000);
 };
