d87c3712fee3f2f88273ed1d12f2196fe0f23f31
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-02-25 15:29:01 +0100

    Add delete bookmark route to nextjs api

--- src/pages/api/shows/bookmarks.ts
+++ src/pages/api/shows/bookmarks.ts
@@ -108,6 +108,64 @@ export default async function handler(
         });
       }
     }
+
+    case "DELETE": {
+      const { showId } = req.body as AddBookmarkBody;
+      const sessionToken = req.cookies["next-auth.session-token"];
+
+      if (!(showId && sessionToken)) {
+        res.status(400).json({
+          message: "Invalid user parameters",
+        });
+        break;
+      }
+
+      const session = await prisma.session.findFirst({
+        where: { sessionToken },
+      });
+
+      if (!session) {
+        res.status(403).json({
+          message: "Unauthenticated user",
+        });
+        break;
+      }
+
+      const showExists = await prisma.show.findFirst({
+        where: { id: showId },
+      });
+      if (!showExists) {
+        res.status(404).json({
+          message: "Show does not exist",
+        });
+        break;
+      }
+
+      const bookmarkExists = await prisma.userBookmarks.findFirst({
+        where: { showId: showId, userId: session.userId },
+      });
+
+      if (!bookmarkExists) {
+        res.status(404).json({
+          message: "Bookmark doesn't exist",
+        });
+        break;
+      }
+
+      try {
+        await prisma.userBookmarks.delete({
+          where: { showId_userId: { userId: session.userId, showId: showId } },
+        });
+
+        res.status(204).json({ message: "Bookmark deleted" });
+        break;
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          message: "Unable to delete bookmark",
+        });
+      }
+    }
     default:
       res.setHeader("Allow", ["POST"]);
       res.status(405).json({
