1b2beb1e261bfbbb828c1a25f2edc3c0424b36fc
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-03-11 03:23:11 +0100

    Remove video previews on hover

--- src/components/Sections/Carousel/CarouselItem/CarouselItem.tsx
+++ src/components/Sections/Carousel/CarouselItem/CarouselItem.tsx
@@ -4,8 +4,6 @@ import CarouselItemText from "./CarouselItemText/CarouselItemText";
 import PlayButton from "./PlayButton";
 import cssClasses from "./CarouselItemImage.module.css";
 import BookmarkButton from "../../BookmarkButton";
-import ReactPlayer from "react-player/youtube";
-import { useState } from "react";
 
 interface CarouselItemProps {
   src: string;
@@ -24,23 +22,7 @@ const CarouselItem: React.FC<CarouselItemProps> = ({
   parentalRating,
   title,
   showId,
-  youtubeUrl,
 }) => {
-  const [isPreviewShowing, setIsPreviewShowing] = useState(false);
-  const [timeOutVariable, setTimeOutVariable] = useState<NodeJS.Timeout>();
-  const isAtleastMediumScreen = useBreakpointValue({
-    base: false,
-    md: true,
-  });
-
-  const setIsPreviewToTrueAfterDelay = () => {
-    setTimeOutVariable(
-      setTimeout(() => {
-        setIsPreviewShowing(true);
-      }, 2000)
-    );
-  };
-
   return (
     <Grid
       className={cssClasses.container}
@@ -52,50 +34,19 @@ const CarouselItem: React.FC<CarouselItemProps> = ({
       templateRows="repeat(16, minmax(0, 1fr))"
       templateColumns="repeat(16, minmax(0, 1fr))"
       position="relative"
-      onMouseEnter={setIsPreviewToTrueAfterDelay}
-      onMouseLeave={() => {
-        clearTimeout(timeOutVariable!);
-        setIsPreviewShowing(false);
-      }}
     >
-      {isPreviewShowing && isAtleastMediumScreen && (
-        <Box
-          gridColumn="1 / -1"
-          gridRow="1 / -1"
-          position="relative"
-          display={{ base: "none", md: "block" }}
-        >
-          <ReactPlayer
-            className="react-player"
-            url={youtubeUrl}
-            height="100%"
-            width="100%"
-            controls={false}
-            playing={true}
-            loop={true}
-            volume={0}
-            muted={true}
-          />
-        </Box>
-      )}
-      {(!isPreviewShowing || !isAtleastMediumScreen) && (
-        <>
-          <BookmarkButton
-            showId={showId}
-            gridRow="2 / 5"
-            gridColumn="14 / 16"
-          />
+      <>
+        <BookmarkButton showId={showId} gridRow="2 / 5" gridColumn="14 / 16" />
 
-          <CarouselItemText
-            year={year}
-            category={category}
-            parentalRating={parentalRating}
-            title={title}
-          />
+        <CarouselItemText
+          year={year}
+          category={category}
+          parentalRating={parentalRating}
+          title={title}
+        />
 
-          <CarouselItemImage src={src} title={title} />
-        </>
-      )}
+        <CarouselItemImage src={src} title={title} />
+      </>
 
       <PlayButton />
     </Grid>
--- src/components/Sections/Gallery/GalleryItem/GalleryItemImage.tsx
+++ src/components/Sections/Gallery/GalleryItem/GalleryItemImage.tsx
@@ -18,22 +18,6 @@ const GalleryItemImage: React.FC<GalleryItemImageProps> = ({
   showId,
   youtubeUrl,
 }) => {
-  const [isPreviewShowing, setIsPreviewShowing] = useState(false);
-  const [timeOutVariable, setTimeOutVariable] = useState<NodeJS.Timeout>();
-  const isAtleastMediumScreen = useBreakpointValue({
-    base: false,
-    md: true,
-  });
-
-  const setIsPreviewToTrueAfterDelay = () => {
-    console.log(youtubeUrl);
-    setTimeOutVariable(
-      setTimeout(() => {
-        setIsPreviewShowing(true);
-      }, 2000)
-    );
-  };
-
   const srcWithStrippedDot = src.replace("./assets", "/assets");
 
   return (
@@ -43,11 +27,6 @@ const GalleryItemImage: React.FC<GalleryItemImageProps> = ({
       templateColumns="repeat(16, minmax(0, 1fr))"
       className={cssClasses.container}
       position="relative"
-      onMouseOver={setIsPreviewToTrueAfterDelay}
-      onMouseOut={() => {
-        clearTimeout(timeOutVariable!);
-        setIsPreviewShowing(false);
-      }}
       // border={{
       //   base: "2px solid purple",
       //   sm: "2px solid pink",
@@ -58,39 +37,17 @@ const GalleryItemImage: React.FC<GalleryItemImageProps> = ({
       //   "2xl": "2px solid green",
       // }}
     >
-      {isPreviewShowing && isAtleastMediumScreen && (
-        <Box gridColumn="1 / -1" gridRow="1 / -1" position="relative">
-          <ReactPlayer
-            className="react-player"
-            url={youtubeUrl}
-            height="100%"
-            width="100%"
-            controls={false}
-            playing={true}
-            loop={true}
-            volume={0}
-            muted={true}
+      <>
+        <BookmarkButton showId={showId} gridRow="2 / 6" gridColumn="13 / 16" />
+        <Box gridRow="1 / -1" gridColumn="1 / -1">
+          <Image
+            src={srcWithStrippedDot}
+            alt={title}
+            fill={true}
+            className={cssClasses.image}
           />
         </Box>
-      )}
-
-      {(!isPreviewShowing || !isAtleastMediumScreen) && (
-        <>
-          <BookmarkButton
-            showId={showId}
-            gridRow="2 / 6"
-            gridColumn="13 / 16"
-          />
-          <Box gridRow="1 / -1" gridColumn="1 / -1">
-            <Image
-              src={srcWithStrippedDot}
-              alt={title}
-              fill={true}
-              className={cssClasses.image}
-            />
-          </Box>
-        </>
-      )}
+      </>
 
       <PlayButton />
     </Grid>
